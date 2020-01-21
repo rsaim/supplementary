882a42537ad1b0e8569ce7dba7b8ca30d7ab1822
Saim Raza <saimorsaim@gmail.com>
2020-01-22 03:36:48 +0530

    Handle cases wherein a long name spans multiple lines.
    A long name that spans multiple lines has all columns except name as NaN. Append this name to
    the name in the previous row. For instance 'KOMARAVOLU NITIN BHARDWAJ' is a single name below.
    
                     name     roll_no MC-301 MC-302 MC-303 MC-304 MC-305 MC-306 MC-307 MC-308 MC-309    TC    SPI     paper_failed
    0       KISHAN ASHIYA  2K12/MC/29     75     63     30     69     68     76     84     72    162  26.0  62.93           MC-303
    1    KOMARAVOLU NITIN  2K12/MC/30     86     69     82     84     89     75     88     87    185  30.0  83.67              NaN
    2            BHARDWAJ         NaN    NaN    NaN    NaN    NaN    NaN    NaN    NaN    NaN    NaN   NaN    NaN              NaN
    3       KRISHNA KUMAR  2K12/MC/31     47     53     48     49     45     60     88     70     95  30.0  53.13              NaN

--- extract.py
+++ extract.py
@@ -2,6 +2,7 @@ import tabula
 import numpy as np
 import pandas as pd
 
+# This will be a list of pd.DataFrame.
 pdf_pages = tabula.read_pdf("/Users/saim/supplementary/some_file.pdf", pages='all')
 
 
@@ -39,9 +40,29 @@ for num, page in enumerate(pdf_pages):
     if 'Max. Marks / Credits' in page.iloc[0].values:
         print(f"Dropping first row {page.iloc[0].values.tolist()!r}")
         page = page.drop(0)
+    page.reset_index(drop=True, inplace=True)
 
     # Remove number and space from the begining of names. Also replace double spaces with a single space.
     page['name'] = page['name'].apply(lambda x: x.strip("0123456789 ").replace("  ", " "))
+
+    # A long name that spans multiple lines has all columns except name as NaN. Append this name to
+    # the name in the previous row. For instance 'KOMARAVOLU NITIN BHARDWAJ' is a single name below.
+    #                  name     roll_no MC-301 MC-302 MC-303 MC-304 MC-305 MC-306 MC-307 MC-308 MC-309    TC    SPI     paper_failed
+    # 0       KISHAN ASHIYA  2K12/MC/29     75     63     30     69     68     76     84     72    162  26.0  62.93           MC-303
+    # 1    KOMARAVOLU NITIN  2K12/MC/30     86     69     82     84     89     75     88     87    185  30.0  83.67              NaN
+    # 2            BHARDWAJ         NaN    NaN    NaN    NaN    NaN    NaN    NaN    NaN    NaN    NaN   NaN    NaN              NaN
+    # 3       KRISHNA KUMAR  2K12/MC/31     47     53     48     49     45     60     88     70     95  30.0  53.13              NaN
+    drop_indices = []
+    for row_num, row in page.iterrows():
+        row_vals = row.values.tolist()
+        row_vals.remove(row['name'])
+        if not any([pd.notnull(val) for val in row_vals]) and row_num > 0:
+            drop_indices.append(row_num)
+            page.loc[row_num - 1, 'name'] = page.loc[row_num - 1, 'name'] + " " + row['name']
+    if drop_indices:
+        page = page.drop(drop_indices)
+        page.reset_index(drop=True, inplace=True)
+
     print(f"Successfully process page {num}...\n")
     print(page)
     sanitized_pages.append(page)
