c411c54c009e920d821fc5ce8c5c6e246a81b668
Saim Raza <saimorsaim@gmail.com>
2020-01-22 02:55:19 +0530

    Add logic to sanitize pdf read tabula.read_pd
    Also, use NameTuple for Subjects. We need to track maximum marks and
    credits for each subject as well.

--- subject_mapping.py
+++ subject_mapping.py
@@ -5,33 +5,35 @@ Contains mapping from Subject code to its canonical name in DTU academic departm
 # Mapping from code to subject of all courses offered in DTU.
 all_courses = {}
 
+import collections
+
+# Declaring namedtuple()
+Subject = collections.namedtuple('Subject', ['name', 'max_marks', 'credits', ])
+
 # Mathematics and Computing courses.
 mc_courses = {
-    'MC-301': 'MODERN ALGEBRA',
-    'MC-302': 'OPERATIONS RESEARCH',
-    'MC-303': 'FINANCIAL ENGINEERING',
-    'MC-304': 'INTERNET & NETWORK SECURITY',
-    'MC-305': 'DATABASE MANAGEMENT SYSTEM',
-    'MC-306': 'DATABASEANAGEMENT SYSTEM LAB',
-    'MC-307': 'OPERATIONS RESEARCH LAB',
-    'MC-308': 'INTERNET & NETWORK SECURITY LAB',
-    'MC-309': 'MINOR PROJECT-I',
+    'MC-301': Subject('MODERN ALGEBRA',                  100, 4),
+    'MC-302': Subject('OPERATIONS RESEARCH',             100, 4),
+    'MC-303': Subject('FINANCIAL ENGINEERING',           100, 4),
+    'MC-304': Subject('INTERNET & NETWORK SECURITY',     100, 4),
+    'MC-305': Subject('DATABASE MANAGEMENT SYSTEM',      100, 4),
+    'MC-306': Subject('DATABASE MANAGEMENT SYSTEM LAB',  100, 2),
+    'MC-307': Subject('OPERATIONS RESEARCH LAB',         100, 2),
+    'MC-308': Subject('INTERNET & NETWORK SECURITY LAB', 100, 2),
+    'MC-309': Subject('MINOR PROJECT-I',                 200, 4)
 }
 all_courses.update(mc_courses)
 
 # Environment Engineering Courses.
 env_courses = {
-    'EN-301': 'WATER SUPPLY & ENVIRONMENTAL SANITATION',
-    'EN-302': 'HEAVY METAL REMOVALS ',
-    'EN-303': 'GEOTECHNICAL ENGINEERING',
-    'EN-304': 'ENVIRONMENTAL HYDRAULICS ',
-    'EN-305': 'INSTRUMENTATION ',
-    'EN-306': 'ENVIRONMENTAL HYDRAULICS LAB',
-    'EN-307': 'GEOTECHNICAL ENGINEERING LAB ',
-    'EN-308': 'INSTRUMENTATION LAB ',
-    'EN-309': 'MINOR PROJECT-I /SURVEYING CAMP EVALUATION',
+    'EN-301': Subject('WATER SUPPLY & ENVIRONMENTAL SANITATION',    100, 4),
+    'EN-302': Subject('HEAVY METAL REMOVALS ',                      100, 4),
+    'EN-303': Subject('GEOTECHNICAL ENGINEERING',                   100, 4),
+    'EN-304': Subject('ENVIRONMENTAL HYDRAULICS',                   100, 4),
+    'EN-305': Subject('INSTRUMENTATION',                            100, 4),
+    'EN-306': Subject('ENVIRONMENTAL HYDRAULICS LAB',               100, 2),
+    'EN-307': Subject('GEOTECHNICAL ENGINEERING LAB',               100, 2),
+    'EN-308': Subject('INSTRUMENTATION LAB ',                       100, 2),
+    'EN-309': Subject('MINOR PROJECT-I /SURVEYING CAMP EVALUATION', 200, 4),
 }
 all_courses.update(env_courses)
-
-
-
