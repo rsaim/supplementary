f5821ba5eba1d143ff58f6906f144c844de6a1f9
Saim Raza <saimorsaim@gmail.com>
2020-01-24 05:44:36 +0530

    Better sanitize filenames.
    Previous sanitization failed with tabula.

--- utils.py
+++ utils.py
@@ -2,11 +2,22 @@ import os
 import re
 
 
+def slugify(value):
+    """
+    Normalizes string, converts to lowercase, removes non-alpha characters,
+    and converts spaces to hyphens.
+
+    Ref: https://stackoverflow.com/a/295466/6463555
+    """
+    value = ascii(value)
+    value = re.sub('[^\w\s-]', '', value).strip()
+    value = re.sub('[-\s]+', '-', value)
+    return value
+
 # Code used to sanitize file names in CWD.
 num_files_before = len(os.listdir("."))
 for filename in os.listdir("."):
-    sanitized_filename = filename.replace("\n", "").strip()
-    sanitized_filename = re.sub(' +', ' ', sanitized_filename).replace(" ", "_")
+    sanitized_filename = slugify(filename)
     print(f"{filename!r} -> {sanitized_filename!r}")
     if os.path.exists(sanitized_filename):
         prefix = 1
