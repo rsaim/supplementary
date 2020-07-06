3814f2d9f1d2b0f3f60ba14fe25f6390654a0844
Saim Raza <saimorsaim@gmail.com>
2020-07-07 02:02:18 +0530

    Add feature to query by rollno from heroku app

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
-from flask import Flask
+import boto3
+import requests
+from flask import Flask, request
+from boto3.dynamodb.conditions import Key
+
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+dynamodb = boto3.resource('dynamodb', region_name="ap-southeast-1")
+table=dynamodb.Table("dturesults")
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    errors = []
+    results = {}
+    student_name = None
+    rollno = None
+    if request.method == "POST":
+        # get rollno that the user has entered
+        try:
+            rollno = request.form['rollno']
+            response = table.query(
+                KeyConditionExpression=Key('rollno').eq(rollno)
+            )
+            item = response["Items"]
+            if not item:
+                errors.append("No record found for rollno={}".format(rollno))
+                roll_no = None
+            else:
+                results = response["Items"][0]
+                student_name = results["name"]
+                roll_no = rollno
+                del results["name"]
+                del results["rollno"]
+        except Exception as err:
+            errors.append(repr(err))
+    return render_template('index.html', errors=errors, results=results, student_name=student_name, roll_no=roll_no)
+
 
 if __name__ == '__main__':
     app.run()
